0bdccfc6665a2c5255d389a7b6fa80e0f46076c1
Ryan Flett <s3633011@student.rmit.edu.au>
2016-11-11 17:26:37 +1100

    Added more users and their groups, as well as an ssh key for a user.

--- modules/userman/manifests/init.pp
+++ modules/userman/manifests/init.pp
@@ -43,12 +43,74 @@
 # Copyright 2016 Ryan Flett
 #
 class userman {
+	group { 'sysadmin':
+		name		=> 'sysadmin',
+		ensure		=> 'present',
+	}
+
+	group { 'cars':
+		name		=> 'cars',
+		ensure		=> 'present',
+	}
+
+	group { 'trucks':
+                name            => 'trucks',
+                ensure          => 'present',
+        }
+
+	group { 'ambulances':
+		name		=> 'ambulances',
+		ensure		=> 'present',
+	}
+
+	file { '/home/becca':
+		ensure		=> 'directory',
+		require		=> User['becca'],
+	}
+
+	file { '/home/fred':
+		ensure		=> 'directory',
+		require		=> User['fred'],
+	}
+
+        file { '/home/wilma':
+                ensure          => 'directory',
+                require         => User['wilma'],
+        }	
+
 	user { 'becca':
 		ensure		=> 'present',
 		uid		=> '10013011',
 		shell		=> '/bin/bash',
+		managehome	=> true,
 		home		=> '/home/becca',
 		groups		=> ['sysadmin', 'cars'],
 		password	=> '$6$9D0f3cny4QKXlSRH$sfHCKWH3T5hzcK1QPjiVbZa1W9x4P4XgYRNoxbbfCeXBDJx/9IzyTrRHHKw1tF3gw/HKZoA1OMtlls1KPxHHR/',
-	}		
+	}
+
+	user { 'fred':
+		ensure		=> 'present',
+		uid		=> '10023011',
+		shell		=> '/bin/csh',
+		managehome	=> true,
+		home		=> '/home/fred',
+		groups		=> ['trucks', 'cars'],
+		password        => '$6$9D0f3cny4QKXlSRH$sfHCKWH3T5hzcK1QPjiVbZa1W9x4P4XgYRNoxbbfCeXBDJx/9IzyTrRHHKw1tF3gw/HKZoA1OMtlls1KPxHHR/',
+	}
+
+        user { 'wilma':
+                ensure          => 'present',
+                uid             => '10033011',
+                managehome      => true,
+                home            => '/home/wilma',
+                groups          => ['trucks', 'cars', 'ambulances'],
+                password        => '$6$9D0f3cny4QKXlSRH$sfHCKWH3T5hzcK1QPjiVbZa1W9x4P4XgYRNoxbbfCeXBDJx/9IzyTrRHHKw1tF3gw/HKZoA1OMtlls1KPxHHR/',
+        }
+
+	ssh_authorized_key { 'wilma_ssh':
+		user		=> 'wilma',
+		type		=> 'rsa',
+		key		=> 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDShliewlx31UPTBS1Gms9mhjOCjOG5Lqh9s1kjafKuk1d2XOiydAK0cf44XKrGWcfx4jBUl2aWD0ZjDRz9GMGXSix6GIP/aDXeATyCckiBs9dZUAKQrd1QoDnl+B8rNp0lmBkipBkoZqrRTPFpfG2iMPC7rAOcOPxlGaThRQYtjDplFFaDUd/chQMq7w/sc67nD4zXlJrE7S1wtJ+10WnZwPu4XJNNkMr9DnaQuQIh9Bvbl5UoWeql3AhAm2GIj54KljXruJ8P7533ddCgFOO320/bRksUey9ofnzGV0vSk3lppsc5+t6qpe2xwCGp0q40eAidd1bNVuaUD7xz/zqZ',
+	}
+
 }
